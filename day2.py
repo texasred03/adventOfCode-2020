afed7f4ac24c00117809aa65b951d0ee42a3c8a9
texasred03 <62958004+texasred03@users.noreply.github.com>
2020-12-03 10:15:20 -0600

    Update day2.py

--- day2.py
+++ day2.py
@@ -4,20 +4,6 @@ textFile = open(r"day2.txt", 'r')
 fileData = textFile.read().splitlines()
 textFile.close()
 
-''' 
-        Sample input
- 1-3 a: abcde
- 1-3 b: cdefg
- 2-9 c: ccccccccc
-
-        Part 1
-The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. 
-For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times 
- 
-        Part 2
-Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on.
-Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
-'''
 part1Total = 0
 part2Total = 0
 for line in fileData:
