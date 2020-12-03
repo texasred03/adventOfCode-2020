c13401b3d68e4c28b187bd4b3df0c18668fc29b3
texasred03 <62958004+texasred03@users.noreply.github.com>
2020-12-03 10:02:38 -0600

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,26 @@
+
+#!/usr/bin/python3
+
+textFile = open(r"day3.txt", 'r')
+fileData = textFile.read().splitlines()
+textFile.close()
+
+def getTreeCount(x, y):
+    trees = 0
+    column = 1
+    for i in range(1, int(len(fileData) / y)):
+        line = fileData[i * y]
+        column = (column + x) % len(line)
+        if line.startswith('#', column-1):
+            trees += 1
+        continue
+    return trees
+
+print("Part1: " + str(getTreeCount(3,1)))
+
+paths = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+
+trees = 1
+for i in range(0, len(paths)):
+    trees *= getTreeCount(paths[i][0], paths[i][1])
+print("Part2: " + str(trees))
