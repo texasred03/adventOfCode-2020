9e0590b88f4ed98b50f3c9d0b72f0d75d0a25056
texasred03 <62958004+texasred03@users.noreply.github.com>
2020-12-08 08:33:29 -0600

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,24 +1,38 @@
-
 #!/usr/bin/python3
+import re
 
 textFile = open(r"day4.txt", 'r')
 fileData = textFile.read().splitlines()
 textFile.close()
 
 def isPassportValid(passport):
+    passportFields = passport.split()
+    
     reqFields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
-    print(passport)
-
-    passportFields = passport.split(" ")
-    if len(passportFields) <= 7 and "cid" in passport:
-        return False
-    #for field in passportFields:
-    #    if field.split(":")[0] not in reqFields:
-            
-            
-    print("valid!")
+    if len(passportFields) <= len(reqFields):
+        if "cid" in passport or len(passportFields) < len(reqFields):
+            return False # Satisfies part 1
+       
+    for field in passportFields:
+        fieldType = field.split(":")[0]
+        data = field.split(":")[1]
+        
+        if fieldType == 'byr' and not re.match("(19[2-8][0-9]|199[0-9]|200[0-2])", data): 
+            return False
+        elif fieldType == 'iyr' and not re.match("(201[0-9]|2020)", data):
+            return False
+        elif fieldType == 'eyr' and not re.match("(202[0-9]|2030)", data):
+            return False
+        elif fieldType == 'hgt':
+            if not re.match("(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in", data):
+                return False
+        elif fieldType == 'hcl' and not re.match("#[a-f0-9]{6}", data):
+            return False
+        elif fieldType == 'ecl' and not re.match("amb|blu|brn|gry|grn|hzl|oth", data):
+            return False
+        elif fieldType == 'pid' and not re.match("^\d{9}$", data):
+            return False
     return True
-
 
 i = 0
 numValidPassports = 0
@@ -31,4 +45,4 @@ while i < len(fileData)-1:
         numValidPassports += 1
     i += 1
 
-print(numValidPassports)
+print("Valid passports: " + str(numValidPassports))
