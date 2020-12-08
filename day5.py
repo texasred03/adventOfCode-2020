c19f28618d0e759a1aea65a78db867de150a6627
texasred03 <tabor.lechner+gh@gmail.com>
2020-12-08 15:54:47 -0600

    adding day 5 part 1

---
+++ day5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+textFile = open(r"day5.txt", 'r')
+fileData = textFile.read().splitlines()
+textFile.close()
+
+def getSeatId(data):
+    binaryData = data.translate(data.maketrans('BRFL','1100')) # Converts characters in string: B/R to 1 and F/L to 0. 
+    row = int(binaryData[0:7],2) # int(number, base) converts a number from specified base. In this example it will convert from binary (base 2)
+    column = int(binaryData[7:10], 2)  
+    return row * 8 + column
+
+seatIds = []
+for inputData in fileData:
+    seatIds.append(getSeatId(inputData))
+    
+print("Highest seat ID: " + str(max(seatIds)))
